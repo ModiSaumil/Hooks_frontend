a819e5cb893442c4b08dc06812117526a6c21b66
ModiSaumil <19bmiit001@gmail.com>
2023-10-26 16:54:17 +0530

    react firsta day

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+  render() {
+    return (
+    <>
+       This is a main root home page.
+    </>
+    )
+  }
+}
+
+export default Home;
