19dd22c7bb7b198589b5343245b712093757f2b0
ModiSaumil <19bmiit001@gmail.com>
2023-10-27 18:10:02 +0530

    react new

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './components/home';
+import Gallery from './components/displaydata';
+import Counter from './components/counter';
+import BucketList from './components/bucketlist';
+import Game from './components/tictaktoe';
 
 
 function App() {
@@ -10,9 +14,10 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home />}></Route>
-            
-
-
+            <Route path='/dd' element={<Gallery/>}></Route>
+            <Route path='/cc' element={<Counter/>}></Route>
+            <Route path='/bb' element={<BucketList/>}></Route>
+            <Route path='/game' element={<Game/>}></Route>
           </Routes>
         </BrowserRouter>
     </div>
